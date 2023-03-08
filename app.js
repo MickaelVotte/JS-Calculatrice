d985bad380d4cb56c6c67a2f7a002564adc4600f
Micka <mickael.votte@hotmail.com>
2023-03-08 18:22:10 +0100

    modification design + fonctionnalité

--- app.js
+++ app.js
@@ -1,66 +1,32 @@
 const afficheCalcul = document.getElementById('calcule');
-const afficheResult = document.getElementById('result');
-const btn = document.querySelectorAll("button");
+const btn = [...document.querySelectorAll("button")];
+
+console.log(btn);
+
+btn.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case'C':
+                afficheCalcul.innerText = "";
+                break;
+            case'DEL':
+                afficheCalcul.innerText = afficheCalcul.innerText.slice(0,-1);
+                break;
+            case'=':
+            try {
+                afficheCalcul.innerText = eval(afficheCalcul.innerText.replace("/[^-+/*\d]/g,"))
+                break;
+            } catch {
+                afficheCalcul.innerText = "Erreur";
+            }
+            default:
+                afficheCalcul.innerText += e.target.innerText
+                break;
+            }
+            console.log(button);
+    })
+})
 
 
 
-let value1 = "";
-let value2 = "";
-let operateur = "";
-let result;
 
-
-
-function chiffre(n) {
-
-    if (operateur === "") {
-        value1 += n
-        console.log(value1);
-    } else if (operateur != "") {
-        value2 += n
-        console.log(value2);
-    }
-
-    afficheCalcul.innerHTML += n
-}
-
-function operator(op) {
-    
-    operateur = op
-   afficheCalcul.innerHTML = op
-
-    console.log(op);
-}
-
-
-function calcul() {
-    switch (operateur) {
-        case '+':
-            result = parseFloat(value1) + parseFloat(value2)
-            console.log(result);
-            break;
-        case '-':
-            result = parseFloat(value1) - parseFloat(value2)
-            console.log(result);
-            break;
-        case '*':
-            result = parseFloat(value1) * parseFloat(value2)
-            console.log(result);
-            break;
-        case '/':
-            result = parseFloat(value1) * parseFloat(value2)
-            console.log(result);
-            break;
-    }
-    afficheResult.innerHTML = result
-
-}
-
-
-function reset() {
-    value1 = "";
-    value2 = "";
-    operateur = "";
-    afficheCalcul.innerHTML ="";
-    afficheResult.innerHTML = "";
-}
